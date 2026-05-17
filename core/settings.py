fb21d2a7bad3b320115651602d907bac3bbd4185
lucas458 <lucasvictor1111@gmail.com>
2026-05-16 22:13:15 -0300

    fix: Corrigir duração do token

--- core/settings.py
+++ core/settings.py
@@ -55,8 +55,10 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 MIDDLEWARE = [
