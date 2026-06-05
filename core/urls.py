cce02ea7a188749624a11100411038d08728ea4a
lucas458 <lucasvictor1111@gmail.com>
2026-06-05 08:47:30 -0300

    fix: Corrigir root do projeto

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import MeView
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "Bem-vindo à API do Journal!", "status": "online"})
 
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/me/', MeView.as_view(), name='me'),
